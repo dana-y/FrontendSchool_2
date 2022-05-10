f74125c5a3779a55fa6c6ff5075a9b3f08526109
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-05-10 23:28:48 +0900

    JS basic

---
+++ 01. 수업내용/JS/002.js
@@ -0,0 +1,4 @@
+document.getElementById("one").innerHTML = "what the hello <strong>document</strong>";
+console.log("what the hello console");
+window.alert("what the hello alert");
+document.write("what the hello write");
