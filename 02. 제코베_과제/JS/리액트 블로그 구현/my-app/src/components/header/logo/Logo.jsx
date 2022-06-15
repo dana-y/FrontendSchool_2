bcd92fb911ba61cf5ae43bcc14ec12c5bd62c7ef
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-06-15 20:28:18 +0900

    Logo 세팅

---
+++ 02. 제코베_과제/JS/리액트 블로그 구현/my-app/src/components/header/logo/Logo.jsx
@@ -0,0 +1,12 @@
+
+const Logo = () => {
+  <div className="max-width">
+    <h1>
+      <a href="./">
+        <img src="../../../assets/Logo.svg" alt="My Blog" />
+      </a>
+    </h1>
+  </div>
+}
+
+export default Logo;
