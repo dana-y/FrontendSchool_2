f6d04bd67c1dbe5572434a5b373043cde2451831
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-06-26 00:14:02 +0900

    props로 data 받아오기 코드 완성

--- 02. 제코베_과제/JS/리액트 블로그 구현/my-app/src/components/Main/Posts/PostList.jsx
+++ 02. 제코베_과제/JS/리액트 블로그 구현/my-app/src/components/Main/Posts/PostList.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useEffect, useState } from "react";
 import styled from 'styled-components';
 import Post from './Post';
 
@@ -14,30 +13,21 @@ const MainUl = styled.ul`
     grid-template-columns: repeat(1, 1fr);
   }
 `
-export default function PostList() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = () => {
-      fetch('/data.json')
-      .then(res => res.json())
-      .then(resData => {
-      setData(resData.posts)
-    })
-  }
-  fetchData();
-	}, [])
+export default function PostList(props) {
 
   return (
+    
     <MainUl>
-      {data.map((item) => (
-        <Post
+      {
+      props.posts.map((item) => (
+        <Post 
           id={item.id}
           category={item.category}
           thumbnailImg={item.thumbnail}
           profileImg={item.profileImg}
           title={item.title}
           created={item.created}
-          userName={item.userName}
+          userName={item.userName} 
         />
       ))}
     </MainUl>
